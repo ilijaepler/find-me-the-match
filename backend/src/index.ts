feef00ca03e9022271c46c709f4dd0c69ba3240b
epler17 <eplerilija17@gmail.com>
2024-02-06 22:28:51 +0100

    user registration api route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import mongoose from 'mongoose';
+import userRouter from './controllers/users';
 
 mongoose.connect(process.env.MONGODB_CONNECTION as string);
 
@@ -10,9 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
 
-app.get("/api/test", async (req: Request, res: Response) => {
-    res.json({message: 'hello from express endpoint!'});
-});
+app.use("/api/users", userRouter);
 
 app.listen(3001, () => {
     console.log('server running on localhost:3001');
